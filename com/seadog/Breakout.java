7c333a604dc2ac3fd009ff27e2fb70aa5c514a01
Seadog1225 <aasssa1225@naver.com>
2020-11-10 10:20:25 +0900

    Make Start Frame

--- com/seadog/Breakout.java
+++ com/seadog/Breakout.java
@@ -4,23 +4,23 @@ import javax.swing.JFrame;
 import java.awt.EventQueue;
 
 public class Breakout extends JFrame {
-    public Breakout() {
-        initUI();
+    public Breakout(int x, int y) {
+        initUI(x, y);
     }
 
-    private void initUI() {
+    private void initUI(int x, int y) {
         add(new Board());
         setTitle("Breakout");
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocation(100, 100);
+        setLocation(x, y);
         setResizable(false);
         pack();
     }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
-            var game = new Breakout();
+            var game = new Breakout(100, 100);
             game.setVisible(true);
         });
     }
---
+++ com/seadog/Start.java
@@ -0,0 +1,77 @@
+package com.seadog;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Start extends JFrame {
+
+    public Start() {
+        setTitle("SBreakout");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 800);
+        setLocation(300, 0);
+        setContentPane(new MyPane());
+        setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new Start();
+    }
+
+    public class MyPane extends JPanel {
+        public MyPane() {
+            setLayout(null);
+            setBackground(Color.LIGHT_GRAY);
+
+            add(new Button1("1"));
+            add(new Button2("2"));
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Arial", Font.ITALIC, 750));
+            g.drawString("S", 200, 150);
+
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("Arial", Font.ITALIC, 70));
+            g.drawString("SBreakout", 150, 225);
+        }
+    }
+
+    public class Button1 extends JButton {
+        public Button1(String s) {
+            setText(s);
+            setSize(150, 50);
+            setLocation(225, 350);
+            this.addMouseListener(new MyMouse());
+        }
+    }
+
+    class MyMouse extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            System.out.println("1 Clicked");
+            int x = getX();
+            int y = getY();
+            dispose();
+            var game = new Breakout(x, y);
+            game.setVisible(true);
+        }
+    }
+
+    public class Button2 extends JButton {
+        public Button2(String s) {
+            setText(s);
+            setSize(150, 50);
+            setLocation(225, 550);
+            this.addMouseListener(new MouseAdapter() {
+                public void mousePressed(MouseEvent e) {
+                    System.out.println("2 Clicked");
+                }
+            });
+        }
+    }
+}
